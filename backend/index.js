b589a22a28ed3d441ca45e5e00fcb3d6d686e8d9
sidhant-kumar08 <162786760+sidhant-kumar08@users.noreply.github.com>
2024-07-07 18:05:18 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const path = require('path')
+
+const PORT = 4000
+
+
+
+//database connection
+mongoose.connect(`mongodb://localhost:27017/node-project`)
+.then(()=>{
+    console.log("mongodb connected");
+}).catch((err)=>{
+    console.log(`error : ${err}`)
+})
+
+
+//userschema
+const userschema = mongoose.Schema({
+    name: String,
+    occupation: String,
+    email: String,
+    age: Number
+})
+
+//model
+const user = mongoose.model("user", userschema)
+
+
+//middlewares
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+//routes
+app.get('/', (req,res)=>{
+    res.render("index")
+});
+
+
+//create user - GET method
+app.get('/create', async (req,res)=>{
+    const formuser = req.query;
+    await user.create(formuser).then(()=>{
+        res.redirect(`/read`)
+    }).catch((err)=>{
+        console.log(`err : ${err}`)
+    })
+});
+
+app.get("/read", async (req,res)=>{
+    
+    let readusers = await user.find({});
+    res.render("read", {user : readusers})
+}
+)
+
+app.get('/delete/:id', async (req,res)=>{
+
+   let deleteUser = await user.findOneAndDelete({_id: req.params.id})
+   res.redirect("/read")
+})
+
+app.get('/edit/:id', async (req,res)=>{
+
+   let editUser = await user.findOne({_id: req.params.id})
+   res.render("edit", {user : editUser})
+})
+
+app.post('/update/:id', async (req,res)=>{
+    let {name, email, age, occupation} = req.body;
+    let editUser = await user.findOneAndUpdate({_id: req.params.id}, {name, email, age, occupation}, {new : true})
+    res.redirect("/read")
+ })
+
+
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`app is listening on port : ${PORT}`)
+})
